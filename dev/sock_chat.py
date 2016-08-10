97a0b9cd915a641ad980d0084d8ac803bd9d657b
Lord AceldmaA <aceldama.v1.0@gmail.com>
2016-08-10 04:52:56 +0100

    Continued building server...

--- dev/sock_chat.py
+++ dev/sock_chat.py
@@ -17,6 +17,7 @@ class Server(object):
     """ A very basic TCP chat server.
         
             RESEARCH:
+                - https://docs.python.org/2/library/socket.html
                 - http://www.binarytides.com/code-chat-application-server-client-sockets-python/
             
             EXPOSES:
@@ -34,19 +35,62 @@ class Server(object):
     """
     
     #-- Constants -----------------------------------------------------------------------------------------------------
-    RECV_BUFFER = 4096 # Advisable to keep it as an exponent of 2
-    PORT = 5000
+    BUFFER_LEN   = 2**12    #-- 4096: Advisable to keep it as an exponent of 2
+    DEFAULT_PORT = 61616
     
     
     #-- Global Vars ---------------------------------------------------------------------------------------------------
     _clients = []
+    _server = None
     
     
     #-- Special Class Methods -----------------------------------------------------------------------------------------
-    def __init__(self):
+    def __init__(self, port=DEFAULT_PORT):
         """ Initializes the object.
         """
-    
+        #-- Initialize the server socket
+        self._server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self._server.bind(("0.0.0.0", port))
+        self._server.listen(10)
+     
+        # Add server socket to the list of readable connections
+        self._clients.append(self._server)
+     
+        print "Server started on port {0}".format(port)
+     
+        while 1:
+            # Get the list sockets which are ready to be read through select
+            read_sockets, write_sockets, error_sockets = select.select(self._clients, [], [])
+     
+            for sock in read_sockets:
+                #New connection
+                if sock == server_socket:
+                    # Handle the case in which there is a new connection recieved through server_socket
+                    sockfd, addr = server_socket.accept()
+                    CONNECTION_LIST.append(sockfd)
+                    print "Client (%s, %s) connected" % addr
+                     
+                    broadcast_data(sockfd, "[%s:%s] entered room\n" % addr)
+                 
+                #Some incoming message from a client
+                else:
+                    # Data recieved from client, process it
+                    try:
+                        #In Windows, sometimes when a TCP program closes abruptly,
+                        # a "Connection reset by peer" exception will be thrown
+                        data = sock.recv(RECV_BUFFER)
+                        if data:
+                            broadcast_data(sock, "\r" + '<' + str(sock.getpeername()) + '> ' + data)                
+                     
+                    except:
+                        broadcast_data(sock, "Client (%s, %s) is offline" % addr)
+                        print "Client (%s, %s) is offline" % addr
+                        sock.close()
+                        CONNECTION_LIST.remove(sock)
+                        continue
+         
+        server_socket.close()    
     
     def __str__(self):
         """ Returns a string representation of the version.
