b22db0c708cd1d59316d9f7f9a3b8c62bfeefe0e
Lord AceldmaA <aceldama.v1.0@gmail.com>
2016-08-14 05:19:54 +0100

    Finally got this asynchronous server working! Thank f*ck for coffee! :D

--- dev/sock_chat.py
+++ dev/sock_chat.py
@@ -9,12 +9,12 @@
 """
 
 #-- Import Dependencies
-import socket, select, time
+import socket, time, select
 from threading import Thread
 
 
 #====================================================================================================[ SERVER CLASS ]==
-class Server(object):
+class Server(Thread):
     """ A very basic TCP chat server.
         
             RESEARCH:
@@ -22,6 +22,7 @@ class Server(object):
                 - http://www.binarytides.com/python-socket-server-code-example/
                 - http://www.binarytides.com/code-chat-application-server-client-sockets-python/
                 - http://www.saltycrane.com/blog/2008/09/simplistic-python-thread-example/
+                - https://pymotw.com/2/asyncore/index.html#module-asyncore
             
             EXPOSES:
                 Constants:
@@ -38,103 +39,133 @@ class Server(object):
     """
     
     #-- Constants -----------------------------------------------------------------------------------------------------
-    DEFAULT_BUFFER  = 2**12         #-- 4096: Advisable to keep it as an exponent of 2
-    DEFAULT_PORT    = 61616
-    DEFAULT_BACKLOG = 5
+    DEFAULT     = { "buffer"    : 2**12,    #-- 4096: Advisable to keep it as an exponent of 2
+                    "port"      : 61616,
+                    "backlog"   : 5 }
     
-    TOKEN_STRING    = ["CONNECT", "READY", "MESSAGE", "DISCONNECT"]
-    CONNECT, READY, MESSAGE, DISCONNECT = range(len(TOKEN_STRING))
+    #TOKEN_STRING    = ["CONNECT", "READY", "MESSAGE", "DISCONNECT"]
+    #CONNECT, READY, MESSAGE, DISCONNECT = range(len(TOKEN_STRING))
     
     
     #-- Global Vars ---------------------------------------------------------------------------------------------------
-    _server_data    = { "port"      : DEFAULT_PORT,
-                        "backlog"   : DEFAULT_BACKLOG,
-                        "buffer"    : DEFAULT_BUFFER }
-    _server_thread = None
-    _callback = None
+    _server_data    = { "port"      : DEFAULT["port"],
+                        "backlog"   : DEFAULT["backlog"],
+                        "buffer"    : DEFAULT["buffer"]     }
+    _server  = None
+    _handler = None
     _running = False
     
     
     #-- Special Class Methods -----------------------------------------------------------------------------------------
-    def __init__(self, port=None, callback=None):
+    def __init__(self, port=None, custom_handler=None):
         """ Initializes the object.
         """
-        #-- Check if a valid port is supplied
+        #-- Inherit Base Class
+        Thread.__init__(self)
+        
+        #-- Daemonize
+        #self.daemon = True
+        
+        #-- Check if a valid custom port is supplied
         if (port is not None) and (type(port) is int) and (port > 0) and (port < 65536):
             self._server_data["port"] = port
         
-        #-- Check if a callback function is supplied
-        if (callback is not None) and hasattr(callback, "__call__"):
-            self._callback = callback
+        #-- Check if a valid custom event handler is supplied
+        if custom_handler is not None:
+            self.handler = custom_handler
         
-    
-    def __str__(self):
-        """ Returns a string representation of the version.
-        """
-        result = ""
-        return result
-        
-    
-    
+            
     #-- Properties ----------------------------------------------------------------------------------------------------
-    #@property
-    #def name(self):
-    #    """ Returns the name. """
-    #    return self._name
-    #@name.setter
-    #def name(self, value):
-    #    """ Sets the name. """
+    @property
+    def port(self):
+        """ Gets the port the server is listening on.
+        """
+        return self._server_data["port"]
+    @port.setter
+    def port(self, value):
+        """ Sets the port the server is listening on.
+        """
+        if (not self._running) and (type(value) is int) and (value < 65536) and (value >= 0):
+            self._server_data["port"] = value
+    
+        
+    @property
+    def handler(self):
+        """ Returns the event handler.
+        """
+        return self._handler
+    @handler.setter
+    def handler(self, value):
+        """ Sets the event handler.
+        """
+        if (value is not None) and hasattr(value, "__call__") and (self._handler is not value):
+            self._handler = value
+    
+        
+    @property
+    def running(self):
+        """ Returns True if the server is running.
+        """
+        return self._running
     
     
     #-- Private Methods -----------------------------------------------------------------------------------------------
-    def _run_server(self):
-        """ Thread runs this to keep server alert or terminate it.
+    def start(self):
+        """ X
         """
+        print " > Server thread started."
+
         #-- Initialize the server socket
-        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         print " > Socket created"
          
         #-- Bind socket to local host and port
         try:
-            server.bind(("0.0.0.0", self._server_data["port"]))
-            print ' > Socket bind complete'
-        except socket.error as msg:
-            print ' > Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-            flag = False
-         
-        if flag:
-            #-- Start listening on socket
-            server.listen(self._server_data["backlog"])
-            print ' > Server now listening for active connections.'
-            
+            print " > Attempting to bind to port..."
+            self._server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self._server.bind(("0.0.0.0", self._server_data["port"]))
+            print ' > Bind successful.'
             self._running = True
-            while self._running:
-                conn, addr = server.accept()
-                print '   - Connected with ' + addr[0] + ':' + str(addr[1])
-                 
-                #start new thread for client.
-                #start_new_thread(clientthread ,(conn,))
-                
-                #self._thread = Thread(target=self._run, args=(self,))
-                #print " > Thread started."
-                time.sleep(50.0/1000.0) #-- 50ms
             
+            #-- Inherit
+            Thread.start(self)
         
-        print " > Server stopped."
-        self._server.close()
+        except socket.error as msg:
+            print " > Bind failed.\n    - Code:    {0}\n    - Message: {1}".format(str(msg[0]), msg[1])
+            
+            #-- Clean up mess
+            self._server.close()
+        
+        
+    def run(self):
+        """ Thread runs this to keep server alert or terminate it.
+        """
+        #-- Start listening on socket
+        self._server.listen(self._server_data["backlog"])
+        print ' > Server now listening for active connections.'
+        
+        _rx = [self._server]
+        _tx = []
+        while self._running:
+            readable, writable, failed = select.select(_rx, [], [])
+            for check in readable:
+                if check is self._server:
+                    conn, addr = self._server.accept()
+                    _rx.append(conn)
+                    print '   - Connected with {0}:{1}'.format(addr[0], str(addr[1]))
+                    #start new thread for client.
+                    #start_new_thread(clientthread ,(conn,))
+                else:
+                    data = check.recv(1024)
+                    check.send(" > Received {0} bytes of data.".format(len(data)))
+                    print " - Client: {0}".format(data)
+                    
+            #time.sleep(2.5)#50.0/1000.0) #-- 50ms
+            #print "    - oopsie loopsie"
     
-        
-    #def _broadcast(self, message):
-    #    """ Sends a message to all connected clients.
-    #    """
-    #    for socket in self._client:
-    #        if socket != server_socket and socket != sock :
-    #            try :
-    #                socket.send(message)
-    #            except :
-    #                # broken socket connection may be, chat client pressed ctrl+c for example
-    #                socket.close()
-    #                CONNECTION_LIST.remove(socket)    
+        self._server.close()
+        print " > Server stopped."
+        print " > Server thread terminated."
     
     
     @staticmethod
@@ -145,84 +176,38 @@ class Server(object):
     
     
     #-- Public Methods ------------------------------------------------------------------------------------------------
-    def start(self):
-        """ Starts the server
+    def send(self, data, client_id=None):
+        """ X
         """
-        flag = True
-    
+        return self, data, client_id
+        
     
     def stop(self):
-        """ Simply stops the server.
+        """ X
         """
-        if self._running:
-            print " > Terminating thread..."
+        if self.running:
+            print " > Shutting down server..."
+            self._server.shutdown(1)
+            self._server.close()
             self._running = False
-    
-    
-    def send(self, client_id, data):
-        """ X
-        """
-        return False
-    
-    
-    def send_all(self, data):
-        """ X
-        """
-        return False
+            print " > Server is now dead."
+        Thread._Thread__stop(self)
+
 
 
 #===========================================================================================================[ DEBUG ]==
 def debug():
     """ Test method. """
-    CONNECTION_LIST = []    # list of socket clients
-    RECV_BUFFER = 4096 # Advisable to keep it as an exponent of 2
-    PORT = 5000
-         
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind(("0.0.0.0", PORT))
-    server_socket.listen(10)
- 
-    # Add server socket to the list of readable connections
-    CONNECTION_LIST.append(server_socket)
- 
-    print "Chat server started on port " + str(PORT)
- 
+    #for f_cknugget in dir(socket):
+    #for f_cknugget in dir(Thread):
+    #    print f_cknugget
+    test = Server()
+    test.start()
     try:
-        while 1:
-            #-- Get the list sockets which are ready to be read through select
-            read_sockets, write_sockets, error_sockets = select.select(CONNECTION_LIST, [], [])
-            for sock in read_sockets:
-                #-- New connection
-                if sock == server_socket:
-                    # Handle the case in which there is a new connection recieved through server_socket
-                    sockfd, addr = server_socket.accept()
-                    CONNECTION_LIST.append(sockfd)
-                    print "Client (%s, %s) connected" % addr
-                     
-                #Some incoming message from a client
-                else:
-                    # Data recieved from client, process it
-                    try:
-                        #In Windows, sometimes when a TCP program closes abruptly,
-                        # a "Connection reset by peer" exception will be thrown
-                        data = sock.recv(RECV_BUFFER)
-                        # echo back the client message
-                        if data:
-                            sock.send('OK ... ' + data)
-                     
-                    # client disconnected, so remove from socket list
-                    except:
-                        broadcast_data(sock, "Client (%s, %s) is offline" % addr)
-                        print "Client (%s, %s) is offline" % addr
-                        sock.close()
-                        CONNECTION_LIST.remove(sock)
-                        continue
-    
+        time.sleep(25)
     except KeyboardInterrupt:
-        print "\n\nUser had enough. Exiting..."
-    
-    server_socket.close()
+        print " > Seems we're terminating early!"
+    test.stop()
 
 
 #============================================================================================================[ MAIN ]==
