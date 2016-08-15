f3039edc72ff5fcb52922b25204609b16528681f
Lord AceldmaA <aceldama.v1.0@gmail.com>
2016-08-15 06:09:24 +0100

    Added a garbage collector to the server, which seems to be working.

--- dev/sock_chat.py
+++ dev/sock_chat.py
@@ -11,7 +11,7 @@
 #-- Import Dependencies
 import socket, time
 from threading import Thread
-#from xifo import Queue
+from xifo import Queue
 
 
 #====================================================================================================[ SERVER CLASS ]==
@@ -43,7 +43,7 @@ class Server(Thread):
         """ X
         """
         #-- Constants ---------------------------------------------------------------------------------------------
-        #[None]
+        BUFFER_SIZE = 2**12     #-- 4096 bytes
         
         
         #-- Global Vars -------------------------------------------------------------------------------------------
@@ -51,13 +51,15 @@ class Server(Thread):
         _addr       = ("0.0.0.0", 0)
         _socket     = None
         _handler    = None
+        _running    = True
+        _queue      = Queue()
         
         
         #-- Special Class Methods ---------------------------------------------------------------------------------
         def __init__(self, client_socket, client_id, client_handler, client_address):
-            """ X
+            """ Starts up and manages the client socket as an individual thread.
             """
-            #-- Inherit Base Class
+            #-- Inherit from Base Class
             Thread.__init__(self)
             
             #-- Initialise globals
@@ -85,35 +87,65 @@ class Server(Thread):
             return self._addr
         
         
-        #@property
-        #def id(self):
-        #    """ Gets the port the server is listening on.
-        #    """
-        #    return self._id
-        #@id.setter
-        #def id(self, value):
-        #    """ Sets the port the server is listening on.
-        #    """
+        @property
+        def running(self):
+            """ Returns True if the client is alive.
+            """
+            return self._running
         
         
         #-- Private Methods ---------------------------------------------------------------------------------------
-        #[None]
+        def _event(self, token, data):
+            """ Raises an event or if no event handler was set it prints to stdout.
+            """
+            if self._handler is None:
+                print "  - ID({3})::{0}({2})> {1}".format(token.upper(), data.strip(), len(data), self._id)
+            else:
+                self._handler(self.id, token.upper(), data)
         
         
         #-- Public Methods ----------------------------------------------------------------------------------------
         def run(self):
-            """ X
+            """ Waits for data from the socket and raises an event if it arrives.
             """
+            self._running = True
+            while self._running:
+                #-- Wait for data from client.
+                data = self._socket.recv(self.BUFFER_SIZE)
+                if data:
+                    #-- Trigger event handler.
+                    self._event("RX", data)
+                else:
+                    #-- Socket closed.
+                    print "KILL KILL KILL!!"
+                    self.kill()
         
         
         def send(self, data):
-            """ X
+            """ Sends data to client.If data is too big, it sends it in chunks.
             """
+            chunk = data
+            while len(chunk):
+                _tx = ""
+                if len(chunk) > self.BUFFER_SIZE:
+                    #-- Chop it up.
+                    _tx = chunk[0:self.BUFFER_SIZE]
+                    chunk = chunk[self.BUFFER_SIZE:]
+                else:
+                    #-- It's small enough. 
+                    _tx = chunk
+                    chunk = ""
+                
+                #-- Send the data chunk.
+                self._event("TX", _tx)
+                self._socket.send(_tx)
         
         
         def kill(self):
-            """ X
+            """ Ends the thread and kills the socket.
             """
+            self._running = False
+            self._socket.close()
     
     
     #-- Constants -----------------------------------------------------------------------------------------------------
@@ -127,13 +159,15 @@ class Server(Thread):
     
     #-- Global Vars ---------------------------------------------------------------------------------------------------
     _clients = []
-    _client_id = 0
+    _client_id = 1
     _server_data    = { "port"      : DEFAULT["port"],
                         "backlog"   : DEFAULT["backlog"],
                         "buffer"    : DEFAULT["buffer"]     }
     _server  = None
     _handler = None
     _running = False
+    
+    _monitor = None
     
     
     #-- Special Class Methods -----------------------------------------------------------------------------------------
@@ -153,6 +187,11 @@ class Server(Thread):
         #-- Check if a valid custom event handler is supplied
         if custom_handler is not None:
             self.handler = custom_handler
+        
+        #-- Start up the garbage collector
+        self._monitor = Thread(target=self._monitor_clients)
+        self._monitor.daemon = True
+        self._monitor.start()
     
     
     #-- Properties ----------------------------------------------------------------------------------------------------
@@ -190,6 +229,67 @@ class Server(Thread):
     
     
     #-- Private Methods -----------------------------------------------------------------------------------------------
+    def _monitor_clients(self):
+        """ Thread that monitors for disconnected clients and cleans them up accordingly.
+        """
+        while True:
+            #-- Find all dead clients
+            lst_pop = []
+            for i in range(len(self._clients)):
+                if not self._clients[i].running:
+                    lst_pop.append(i)
+            
+            #-- Remove them
+            while len(lst_pop):
+                print "DEAD:", self._clients.pop(lst_pop.pop()).id
+            
+            #-- Wait a few seconds before scanning again
+            time.sleep(3)
+    
+    
+    def _reset(self):
+        """ Resets the global variables responsible for client tracking etc.
+        """
+        if self._running():
+            #-- Stop first
+            self.stop()
+        
+        #-- Reset globals
+        self._clients   = []
+        self._client_id = 1
+        
+    
+    
+    def _event(self, sender_id, token, data):
+        """ X
+        """
+    
+    
+    def run(self):
+        """ Thread runs this to keep server alert or terminate it.
+        """
+        #-- Start listening on socket
+        self._server.listen(self._server_data["backlog"])
+        print ' > Server now listening for active connections.'
+        
+        while self._running:
+            (client_socket, address) = self._server.accept()
+            self._clients.append(Server._Client(client_socket, self._client_id, None, tuple(address)))
+            self._client_id += 1
+        
+        self._server.close()
+        print " > Server stopped."
+        print " > Server thread terminated."
+    
+    
+    @staticmethod
+    def _spit(text):
+        """ Just prints a message to stdout.
+        """
+        print text
+    
+    
+    #-- Public Methods ------------------------------------------------------------------------------------------------
     def start(self):
         """ X
         """
@@ -217,51 +317,6 @@ class Server(Thread):
             self._server.close()
         
         
-    def run(self):
-        """ Thread runs this to keep server alert or terminate it.
-        """
-        #-- Start listening on socket
-        self._server.listen(self._server_data["backlog"])
-        print ' > Server now listening for active connections.'
-        
-        while self._running:
-            (client_socket, address) = self._server.accept()
-            self._clients.append(Server._Client(client_socket, self._client_id, None, tuple(address)))
-        
-        #_rx = [self._server]
-        #while self._running:
-        #    readable, writable, failed = select.select(_rx, [], [])
-        #    for check in readable:
-        #        if check is self._server:
-        #            conn, addr = self._server.accept()
-        #            _rx.append(conn)
-        #            print '   - Connected with {0}:{1}'.format(addr[0], str(addr[1]))
-        #            #start new thread for client.
-        #            #start_new_thread(clientthread ,(conn,))
-        #        else:
-        #            data = check.recv(self._server_data["buffer"])
-        #            check.send(" > Received {0} bytes of data.".format(len(data)))
-        #            print " - Client: {0}".format(data)
-        #    
-        #    for check in writable:
-        #        print check
-        #    
-        #    for check in failed:
-        #        print check
-        
-        self._server.close()
-        print " > Server stopped."
-        print " > Server thread terminated."
-    
-    
-    @staticmethod
-    def _spit(text):
-        """ Just prints a message to stdout.
-        """
-        print text
-    
-    
-    #-- Public Methods ------------------------------------------------------------------------------------------------
     def send(self, data, client_id=None):
         """ X
         """
@@ -280,22 +335,19 @@ class Server(Thread):
             self._running = False
             print " > Server is now dead."
         
-        self._Thread__stop(self) # pylint: disable=no-member
+        self._Thread__stop() # pylint: disable=no-member
 
 
 
 #===========================================================================================================[ DEBUG ]==
 def debug():
     """ Test method. """
-    #for f_cknugget in dir(socket):
-    #for f_cknugget in dir(Thread):
-    #    print f_cknugget
-    #print type(Thread._Thread__stop)
-    
     test = Server()
     test.start()
     try:
-        time.sleep(25)
+        for i in range(6):
+            test.send(["ah", "stayin' alive"][int(i / 5.0)])
+            time.sleep(4)
     except KeyboardInterrupt:
         print "\r > Seems we're terminating early!"
     test.stop()
